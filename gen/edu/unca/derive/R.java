ff8519cd5ab0382378f8bca2efb11a96aed2e9e7
msolle <msolle90@gmail.com>
2014-03-14 17:28:30 -0400

    added custom xml for list items

--- gen/edu/unca/derive/R.java
+++ gen/edu/unca/derive/R.java
@@ -32,6 +32,7 @@ public final class R {
     public static final class layout {
         public static final int activity_fragment=0x7f030000;
         public static final int fragment_derive=0x7f030001;
+        public static final int list_item_derive=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f050000;
