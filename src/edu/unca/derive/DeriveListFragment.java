a18e5e0239e6e4ada0f6d11ef28e36a6095669aa
msolle <msolle90@gmail.com>
2014-03-14 17:45:16 -0400

    changed DeriveListFragment to populate from custom adapter

--- src/edu/unca/derive/DeriveListFragment.java
+++ src/edu/unca/derive/DeriveListFragment.java
@@ -6,8 +6,11 @@ import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class DeriveListFragment extends ListFragment {
 	private ArrayList<Derive> mDerives;
@@ -19,12 +22,12 @@ public class DeriveListFragment extends ListFragment {
 		getActivity().setTitle(R.string.derives_title);
 		mDerives = DeriveList.get(getActivity()).getDerives();
 		
-		ArrayAdapter<Derive> adapter = new ArrayAdapter<Derive>(getActivity(), android.R.layout.simple_list_item_1, mDerives);
+		DeriveAdapter adapter = new DeriveAdapter(mDerives);
 		setListAdapter(adapter);
 	}
 	
 	public void onListItemClick(ListView l, View v, int position, long id) {
-		Derive d = (Derive)(getListAdapter()).getItem(position);
+		Derive d = ((DeriveAdapter)getListAdapter()).getItem(position);
 		Log.d(TAG, d.getTitle() + " was clicked");
 	}
 	
@@ -32,6 +35,31 @@ public class DeriveListFragment extends ListFragment {
 		
 		public DeriveAdapter(ArrayList<Derive> derives) {
 			super(getActivity(), 0, derives);
+		}
+		
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			//Inflate view in none passed
+			if(convertView == null) {
+				convertView = getActivity().getLayoutInflater().inflate(R.layout.list_item_derive, null);				
+			}
+			
+			//Configure for Derive
+			Derive d = getItem(position);
+			
+			//Set Title
+			TextView titleTextView = (TextView)convertView.findViewById(R.id.derive_list_item_titleTextView);
+			titleTextView.setText(d.getTitle());
+			
+			//Set Date
+			TextView dateTextView = (TextView)convertView.findViewById(R.id.derive_list_item_dateTextView);
+			dateTextView.setText(d.getDate().toString());
+			
+			//Set check box
+			CheckBox doneCheckBox = (CheckBox)convertView.findViewById(R.id.derive_list_item_doneCheckBox);
+			doneCheckBox.setChecked(d.isDone());
+			
+			return convertView;
 		}
 		
 	}
